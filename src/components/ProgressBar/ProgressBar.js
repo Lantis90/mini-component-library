624f123dce20c6ffab58b5058b305d71bccede51
Santiago Ramos <santicmb@gmail.com>
2025-07-12 19:46:36 -0300

    Ex 1: Progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,64 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZES = {
+  small: {
+    "--height": "8px",
+    "--borderRadius": "4px",
+    "--padding": "0px",
+  },
+  medium: {
+    "--height": "12px",
+    "--borderRadius": "4px",
+    "--padding": "0px",
+  },
+  large: {
+    "--height": "24px",
+    "--borderRadius": "8px",
+    "--padding": "4px",
+  },
+};
+
+const ProgressBarContainer = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: var(--borderRadius);
+  padding: var(--padding);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const ProgressBarFill = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${(props) => props.value}%;
+  height: var(--height);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  /* Trims off corners when progress is almost full */
+  overflow: clip;
+`;
+
+const ProgressBar = ({ value = 0, size = "medium" }) => {
+  const styles = SIZES[size];
+
+  return (
+    <ProgressBarContainer
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={styles}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarWrapper>
+        <ProgressBarFill style={styles} value={value} />
+      </BarWrapper>
+    </ProgressBarContainer>
+  );
 };
 
 export default ProgressBar;
